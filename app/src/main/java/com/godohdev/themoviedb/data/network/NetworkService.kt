81118960fc4fef726e5de1d26ec7fd256b478332
Wahyu Permadi <wahyu.permadi1725@gmail.com>
2020-02-20 15:39:19 +0700

    refactor(app-module): add network service and network module

---
+++ app/src/main/java/com/godohdev/themoviedb/data/network/NetworkService.kt
@@ -0,0 +1,12 @@
+package com.godohdev.themoviedb.data.network
+
+/**
+ *
+ * Created by Wahyu Permadi on 2020-02-20.
+ * Android Engineer
+ *
+ **/
+
+interface NetworkService {
+
+}
--- app/src/main/java/com/godohdev/themoviedb/di/modul/NetworkModule.kt
+++ app/src/main/java/com/godohdev/themoviedb/di/modul/NetworkModule.kt
@@ -1,8 +1,61 @@
 package com.godohdev.themoviedb.di.modul
 
+import android.util.Log
+import com.godohdev.themoviedb.BuildConfig
+import com.godohdev.themoviedb.data.network.NetworkService
+import com.google.gson.Gson
+import dagger.Module
+import dagger.Provides
+import okhttp3.Dispatcher
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
 /**
  *
  * Created by Wahyu Permadi on 2020-02-20.
  * Android Engineer
  *
  **/
+
+@Module
+class NetworkModule {
+
+    @Provides
+    @Singleton
+    fun gson(): Gson = Gson()
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
+        val httpInterceptor = HttpLoggingInterceptor(HttpLoggingInterceptor.Logger {
+            Log.d("Movie", it)
+        })
+
+        httpInterceptor.level = HttpLoggingInterceptor.Level.BASIC
+
+        return OkHttpClient.Builder()
+            .addInterceptor(httpInterceptor)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRestClient(okHttpClient: OkHttpClient) : Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BuildConfig.BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideNetworkService(retrofit: Retrofit): NetworkService{
+        return retrofit.create(NetworkService::class.java)
+    }
+
+}
